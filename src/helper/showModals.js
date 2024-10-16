8c87a7ad826d639b6d0b33d4556fdde5040908f6
Braian Palacios <braianpalacios699@gmail.com>
2024-10-16 12:31:19 -0300

    se agrego logica de iconos de cv

--- src/helper/showModals.js
+++ src/helper/showModals.js
@@ -95,6 +95,56 @@ export const showModalsMessageAlert = (indexMessage) => {
       e.preventDefault();
       updateClassName("add", modalContainer, null, "modal--hidden");
     }
+
+    const up = "bi-caret-up-fill",
+      down = "bi-caret-down-fill";
+
+    let isCv = e.target.textContent === "CV";
+    let isCarteIcon =
+      e.target.matches(`.${up}`) || e.target.matches(`.${down}`);
+
+    // LOCALIZAMOS ICONOS PARA DESPLEGAR.
+    const iconUp = d.querySelector(`.${up}`),
+      iconDown = d.querySelector(`.${down}`);
+
+    if (e.target.matches("#cv-en")) dowloadCv(e); // ->SI CONTIENE EL ID CORRESPONDIENTE cv-en
+
+    if (e.target.matches("#cv-es")) dowloadCv(e); // ->SI CONTIENE EL ID CORRESPONDIENTE cv-es
+
+    // --> SI CV ES TRUE Y SI ES LA CLASE CORRESPONDIENTE DE ICONOS A EVALUAR
+    if (isCv || isCarteIcon) {
+      e.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+      updateClassName("toggle", subMenuCv, null, "sub-menu__cv--show"); //FUNCION PARA ACCIONES DE CLASSLIST.
+
+      // SI ES EL ICONO HACIA ARRIBA
+      if (iconUp) updateClassName("replace", iconUp, down, up);
+
+      // SI ES EL ICONO HACIA ABAJO
+      if (iconDown) updateClassName("replace", iconDown, up, down);
+    }
+
+    // SI CONTIENEN CLASE .bi-list
+    if (e.target.matches(".bi-list")) {
+      updateClassName(
+        "toggle",
+        navigation,
+        null,
+        "container-header__nav--show"
+      );
+      updateClassName("toggle", iconClosed, null, "bi-x-square-fill--hide");
+      updateClassName("toggle", e.target, null, "bi-list--hide");
+    }
+    // SI CONTIENEN CLASE .bi-x-square-fill
+    if (e.target.matches(".bi-x-square-fill")) {
+      updateClassName(
+        "toggle",
+        navigation,
+        null,
+        "container-header__nav--show"
+      );
+      updateClassName("toggle", iconOpen, null, "bi-list--hide");
+      updateClassName("toggle", iconClosed, null, "bi-x-square-fill--hide");
+    }
     // smoothScrollToSection(e);
   });
 };
